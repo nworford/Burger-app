4c24a8e477bf30c973245c574d50baa6aafb2eca
nlw113 <nathanworford@gmail.com>
2018-03-05 19:00:34 -0700

    moved sql connection to server file

--- config/connections.js
+++ config/connections.js
@@ -21,9 +21,6 @@ if(process.env.JAWSDB_URL) {
 //     database:"burgers_db"
 //   });
   
-  connection.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-  });
+  
 
   module.exports = connection;
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var routes = require("./controllers/burgers_controllers.js");
 var app = express();
 // const PORT = process.env.PORT || 3000;
 var PORT = process.env.PORT || 3000;
-
+var connection = require('./config/connections.js');
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
@@ -25,8 +25,17 @@ app.set('port', process.env.PORT || 3000);
 //     console.log("listening on port " + server.address().port);
 // });
 
-app.listen(PORT, (err)=>{
+connection.connect(function(err) {
+    if (err) throw err;
     if(!err) {
-        console.log('listening on port: ' + PORT);
+        app.listen(PORT, (err)=>{
+            if(!err) {
+                console.log('listening on port: ' + PORT);
+            }
+            });
     }
-    });
+    console.log("Connected!");
+  });
+
+
+
